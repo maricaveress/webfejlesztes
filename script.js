408a68d691ba2f6d86d1f6aa238ab9d4e638f901
maricaveress <153008546+maricaveress@users.noreply.github.com>
2023-12-07 05:17:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const navLinks = document.querySelector('.nav-links');
+document.addEventListener('DOMContentLoaded', function () {
+    const shareButtonContainer = document.getElementById('megosztas');
+    const messageInput = document.getElementById('uzenet');
+    const url = window.location.href;
 
-    hamburgerMenu.addEventListener('click', function() {
-        navLinks.classList.toggle('show');
-        this.classList.toggle('open');
-    });
-});*/
+    // Check if the Web Share API is supported
+    if (navigator.share) {
+        createShareButton('Megosztás', shareUsingWebShareAPI);
+    } else {
+        console.log('Web Share API is not supported on this browser.');
+        createSocialMediaShareButton('Facebook');
+        createSocialMediaShareButton('Twitter');
+        createSocialMediaShareButton('Instagram'); // Added Instagram button
+    }
 
+    function createShareButton(label, clickHandler) {
+        const button = document.createElement('button');
+        button.innerText = label;
+        button.addEventListener('click', clickHandler);
+        shareButtonContainer.appendChild(button);
+    }
 
-/*ötletek:
-- űrlap kérdéseit értékeli ki egyből
+    function createSocialMediaShareButton(socialMedia) {
+        createShareButton(socialMedia, function () {
+            shareUsingSocialMedia(socialMedia);
+        });
+    }
 
-*/
+    function shareUsingWebShareAPI() {
+        navigator.share({
+            title: 'Your Exchange Program',
+            text: messageInput.value || 'Check out this exchange program!',
+            url: url
+        })
+        .then(() => console.log('Successfully shared'))
+        .catch((error) => console.error('Error sharing:', error));
+    }
+    function shareUsingSocialMedia(socialMedia) {
+        let socialMediaUrl;
+    
+        switch (socialMedia) {
+            case 'Facebook':
+                socialMediaUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url);
+                break;
+            case 'Twitter':
+                socialMediaUrl = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(messageInput.value || 'Check out this exchange program!');
+                break;
+            case 'Instagram':
+                // Adjust this link according to your Instagram sharing needs
+                socialMediaUrl = 'https://www.instagram.com/';
+                break;
+            default:
+                break;
+        }
 
-
-
-$(document).ready(function () {
-    $("#feliratkozas").validate({
-        rules: {
-            knev: "required",
-            email1: {
-                required: true,
-                email: true,
-            },
-            email2: {
-                required: true,
-                email: true,
-            },
-            feltetelek: "required",
-        },
-        messages: {
-            knev: "Add meg a neved!",
-            email1: "Add meg az email címed!",
-            email2:"Add meg az email címed még egyszer!",
-        },
-
-    });
+        if (socialMediaUrl) {
+            window.open(socialMediaUrl, '_blank', 'width=600,height=400');
+        }
+    }
 });
